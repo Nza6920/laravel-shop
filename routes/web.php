b6deee959e674ea19941d9a21c5c73dd42937b82
Niu zian <2388426660@qq.com>
2018-07-25 06:47:32 +0000

    邮箱验证中间件

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,16 @@ Route::get('/', 'PagesController@root')->name('root');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::group(['middleware' => 'auth'], function() {
+    Route::get('/email_verify_notice', 'PagesController@emailVerifyNotice')->name('email_verify_notice');
+
+    // star
+    Route::group(['middleware' => 'email_verified'], function() {
+        Route::get('/test', function() {
+          return 'ok!';
+        });
+    });
+    //end
+
+});
