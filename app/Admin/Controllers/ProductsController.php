90c6695636773e26d216163a18b136938ffc903f
Niu zian <2388426660@qq.com>
2018-07-26 08:55:13 +0000

    管理后台 - 商品列表

---
+++ app/Admin/Controllers/ProductsController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Admin\Controllers;
+
+use App\Models\Product;
+
+use Encore\Admin\Form;
+use Encore\Admin\Grid;
+use Encore\Admin\Facades\Admin;
+use Encore\Admin\Layout\Content;
+use App\Http\Controllers\Controller;
+use Encore\Admin\Controllers\ModelForm;
+
+class ProductsController extends Controller
+{
+    use ModelForm;
+
+    public function index()
+    {
+        return Admin::content(function (Content $content) {
+            $content->header('商品列表');
+            $content->body($this->grid());
+        });
+    }
+
+    protected function grid()
+    {
+      return Admin::grid(Product::class, function (Grid $grid) {
+           $grid->id('ID')->sortable();
+           $grid->title('商品名称');
+           $grid->on_sale('已上架')->display(function ($value) {
+               return $value ? '是' : '否';
+           });
+           $grid->price('价格');
+           $grid->rating('评分');
+           $grid->sold_count('销量');
+           $grid->review_count('评论数');
+
+           $grid->actions(function ($actions) {
+               $actions->disableDelete();
+           });
+           $grid->tools(function ($tools) {
+               // 禁用批量删除按钮
+               $tools->batch(function ($batch) {
+                   $batch->disableDelete();
+               });
+           });
+       });
+   }
+}
