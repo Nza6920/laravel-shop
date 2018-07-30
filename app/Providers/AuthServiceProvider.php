28eaf26391624e826ee1480f0ff0ae38c09c7f5d
Niu zian <2388426660@qq.com>
2018-07-30 08:58:48 +0000

    用户界面 - 订单详情页

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -5,12 +5,15 @@ namespace App\Providers;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use App\Policies\UserAddressPolicy;
+use App\Policies\OrderPolicy;
 use App\Models\UserAddress;
+use App\Models\Order;
 
 class AuthServiceProvider extends ServiceProvider
 {
     protected $policies = [
         UserAddress::class => UserAddressPolicy::class,
+        Order::class       => OrderPolicy::class;
     ];
 
     public function boot()
