df2dd1a153e0af59ff40585caa44571ce0795ca5
Niu zian <2388426660@qq.com>
2018-08-08 02:49:31 +0000

    管理后台 - 新增, 修改, 删除优惠券

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -16,7 +16,12 @@ Route::group([
     $router->put('products/{id}', 'ProductsController@update');
     $router->get('coupon_codes', 'CouponCodesController@index');
     $router->get('products/create', 'ProductsController@create');
+    $router->post('coupon_codes', 'CouponCodesController@store');
     $router->get('products/{id}/edit', 'ProductsController@edit');
+    $router->put('coupon_codes/{id}', 'CouponCodesController@update');
+    $router->get('coupon_codes/create', 'CouponCodesController@create');
+    $router->get('coupon_codes/{id}/edit', 'CouponCodesController@edit');
+    $router->delete('coupon_codes/{id}', 'CouponCodesController@destroy');
     $router->get('orders', 'OrdersController@index')->name('admin.orders.index');
     $router->get('orders/{order}', 'OrdersController@show')->name('admin.orders.show');
     $router->post('orders/{order}/ship', 'OrdersController@ship')->name('admin.orders.ship');
